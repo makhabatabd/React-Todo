8865132397bc2c8465125c76d08444a73614ff1e
makhabatabd <makhabat9898@gmail.com>
2023-02-05 13:36:42 +0600

    final commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const addTask = (userInput) => {
+    if (userInput) {
+      const newTask = {
+        id: Date.now(),
+        task: userInput,
+        completed: false,
+      };
+      setTodos([...todos, newTask]);
+    }
+  };
+  const removeTask = (id) => {
+    setTodos([...todos.filter((todo) => todo.id !== id)]);
+  };
+
+  const toggleTask = (id) => {
+    setTodos([
+      ...todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : { ...todo }
+      ),
+    ]);
+  };
   return (
-    <div className="App">
-      Hello
+    <div className="todo-app">
+      <h1>Todo List</h1>
+      <TodoForm addTask={addTask} />
+      <hr className="separator" />
+      {todos.map((todo) => (
+        <TodoItem
+          todo={todo}
+          key={todo.id}
+          removeTask={removeTask}
+          toggleTask={toggleTask}
+        />
+      ))}
     </div>
   );
 }
